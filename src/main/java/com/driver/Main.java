c3f0f0b8fdac5fedbdfcc337c9fa5a489f1164be
itsviveksinghania <vickykumarsify@gmail.com>
2023-01-26 13:46:35 +0530

    hi

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -6,3 +6,21 @@ public class Main {
         System.out.println(obj.meth());
     }
 }
+
+class A {
+    public A() {
+    }
+    String meth(){
+        return "Invoking method from class A";
+    }
+}
+
+class B extends A{
+    public B() {
+    }
+
+    @Override
+    String meth() {
+        return "Method is overridden in Extendend class B";
+    }
+}
